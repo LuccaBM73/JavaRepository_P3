49ab167f6dd4754b67aace1c5bf822a46f836311
LuccaBM73 <43149281+LuccaBM73@users.noreply.github.com>
2019-04-24 11:43:40 -0300

    att URI codes

---
+++ URI/1007.java
@@ -0,0 +1,26 @@
+package javainicial;
+import java.util.Scanner;
+/*	24/04/2019 - 11:24
+ *  URI -Lucca BM
+ *  GITHUB - github.com/LuccaBM73/
+ *  
+ */
+public class Aula1 
+{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int A = input.nextInt();
+		int B = input.nextInt();
+		int C = input.nextInt();
+		int D = input.nextInt();
+		int DIFERENCA = ((A*B)-(C*D));
+		System.out.printf("DIFERENCA  = "+DIFERENCA );
+		
+		
+		
+		System.out.println("\nFim da linha");
+	}
+	
+	
+}
